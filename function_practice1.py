4964bfa6a33a99687c2d218e71c20f8ab2646da3
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-17 01:59:33 +0900

    Update function_practice1.py

--- function_practice1.py
+++ function_practice1.py
@@ -1,10 +1,15 @@
 # 4-1 문제 1
 
+def is_natural_number(n):
+    if type(n) == int and n > 0:
+        return True
+
 def is_odd(n):
-    if type(n) != int or n <= 0:
+    if is_natural_number(n) == False:
         print('자연수를 입력해주십시오')
+        return
+    
+    if n % 2 == 0:
+        print('%d = 짝수' % n)
     else:
-        if n % 2 == 0:
-            print('%d = 짝수' % n)
-        else:
-            print('%d = 홀수' % n)
+        print('%d = 홀수' % n)
