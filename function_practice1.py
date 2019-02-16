39c5540c99dbc0d4c88a6d54b6c487f96f1239f3
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-17 00:04:01 +0900

    Update function_practice1.py

--- function_practice1.py
+++ function_practice1.py
@@ -1,9 +1,7 @@
 # 4-1 문제 1
 
 def is_odd(n):
-    if type(n) != int:
-        print('자연수를 입력해주십시오')
-    elif n <= 0:
+    if type(n) != int or n <= 0:
         print('자연수를 입력해주십시오')
     else:
         if n % 2 == 0:
