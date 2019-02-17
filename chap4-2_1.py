4d5fe38f7de0f2eac7253b80c4a4050d13ef6881
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-17 21:41:54 +0900

    Update chap4-2_1.py

--- chap4-2_1.py
+++ chap4-2_1.py
@@ -1,9 +1,9 @@
 # 4-2 문제 1
 
-input1 = input("첫번째 숫자를 입력하세요:")
-input2 = input("두번째 숫자를 입력하세요:")
-input1 = int(input1)
-input2 = int(input2)
+raw_input1 = input("첫번째 숫자를 입력하세요:")
+raw_input2 = input("두번째 숫자를 입력하세요:")
+input1 = int(raw_input1)
+input2 = int(raw_input2)
 
 total = input1 + input2
 print("두 수의 합은 %s 입니다" % total)
