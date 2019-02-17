63a4dd37d227ab5980b8cb991a1e40956f681a6c
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-17 21:52:10 +0900

    Update chap4-2_3.py

--- chap4-2_3.py
+++ chap4-2_3.py
@@ -1,15 +1,15 @@
 # 4-2 문제 4
-end_bool = False
+finished = False
 
-while end_bool == False:
+while not finished:
     raw_input = input("2부터 9까지의 숫자 중 하나를 입력하세요:")
 
-    sample1 = ['2', '3', '4', '5', '6', '7', '8', '9']
-    if raw_input not in sample1:
+    sample = '23456789'
+    if raw_input not in sample:
         continue
 
     input = int(raw_input)
-    for i in range(1,10):
-        print(input*i, end = ' ')
-
-    end_bool = True
+    for i in range(1, 10):
+        print(input * i, end = ' ')
+    break
+ 
