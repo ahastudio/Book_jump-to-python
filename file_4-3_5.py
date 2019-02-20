ccf419617a06be78041082c1d6e0d7ea6a98676e
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-20 10:30:43 +0900

    Update file_4-3_5.py

--- file_4-3_5.py
+++ file_4-3_5.py
@@ -1,11 +1,9 @@
 # 4-3 Q5
 
-with open("sample.txt", 'r') as f1:
-    raw_data = f1.read()
-    data = raw_data.split()
-    numbers = [int(i) for i in data]
-    sum_result = sum(numbers)
-    average = sum_result / len(numbers)
-
-with open("result.txt", 'w') as f2:
-    f2.write(str(average))
+with open("sample.txt", 'r') as f:
+    numbers = [int(i) for i in f.readlines()]
+    
+average = sum(numbers) / len(numbers)
+    
+with open("result.txt", 'w') as f:
+    f.write(str(average))
