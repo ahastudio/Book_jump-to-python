e2e73c65502b6039f4ed138953a196ffe4a905e0
YongjoonSeo <47657852+YongjoonSeo@users.noreply.github.com>
2019-02-20 10:08:13 +0900

    Update file_4-3_3.py

--- file_4-3_3.py
+++ file_4-3_3.py
@@ -1,9 +1,9 @@
 # 4-3 Q3
-f1 = open("C:/Users/15U560/Desktop/2019-1/python/workspace/pythonbook/abc.txt", 'r')
+f1 = open("./abc.txt", 'r')
 lines = f1.readlines()
 f1.close()
 
-f2 = open("C:/Users/15U560/Desktop/2019-1/python/workspace/pythonbook/abc.txt", 'w')
-for i in range(0, len(lines)):
-    f2.write(lines[-(1+i)])
+f2 = open("./abc.txt", 'w')
+for line in lines[::-1]:
+    f2.write(line)
 f2.close()
